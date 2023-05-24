4ad1c73313ba477e199c2cfef25a89aa2a4ec2e1
Karai2mil <128420458+Karai2mil@users.noreply.github.com>
2023-05-24 02:06:43 +0000

    agregamos todo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["dark", "big", "funny", "abominable"];
+  let noun = ["man", "womoan", "clown", "rodent", "animal", "famous"];
+  let end = [".com", ".es", ".uy", ".br", ".io"];
+
+  let listOfDomains = [];
+
+  for (let pronouns of pronoun) {
+    for (let adjetives of adj) {
+      for (let nouns of noun) {
+        for (let ends of end) {
+          listOfDomains.push(pronouns + adjetives + nouns + ends);
+        }
+      }
+    }
+  }
+
+  let randomDomainName =
+    listOfDomains[Math.floor(Math.random() * listOfDomains.length - 1)];
+
+  document.getElementById("hola").innerHTML = randomDomainName;
+  document.getElementById("button").addEventListener("click", function() {
+    location.reload();
+    alert("I'll give you a better one O|");
+  });
 };
